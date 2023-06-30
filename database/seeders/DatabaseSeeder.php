6171bd02e9e144a60bf9d4fe630a814901fdef3b
Mauricio <sir.silvabmauricio@gmail.com>
2023-06-29 23:53:05 -0400

    feat: add product seedder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(CategorySeeder::class);
         $this->call(SupplierSeeder::class);
+        $this->call(ProductSeeder::class);
     }
 }
